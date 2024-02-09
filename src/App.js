562e95048c41a0fe36251c7b21ba5883213de018
unknown <krishnan.subramanian@spit.ac.in>
2024-02-09 20:01:36 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Login from './Screens/Login';
 import Signup from './Screens/Signup';
+import Home from "./Screens/Home";
 function App() {
   return (
     <div className="App">
      <Router>
           <Routes>
-          {/* <Route exact path='/' element = {<Home/>}/> */}
+          <Route exact path='/' element = {<Home/>}/>
           <Route exact path='/signup' element = {<Signup/>}/>
           <Route exact path='/login' element = {<Login/>}/>
           </Routes>
---
+++ src/rootSVG.jsx
@@ -0,0 +1,7 @@
+import { styled, Theme } from '@mui/material/styles';
+
+const Svg = styled('svg')({
+  verticalAlign: 'bottom',
+});
+
+export default Svg;
